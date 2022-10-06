41dd3a1a057dcd6333934c862bbb3a0ad12c1dec
Joshua Prettyman <j.prettyman@icloud.com>
2022-10-06 16:27:52 +0200

    added column_ref.txt

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
+import pandas as pd
 from .client import SemRushClient
+
+
+with open('column_ref.txt', "r") as f:
+    column_ref_string = f.read()
+_rows = [_.split('\t') for _ in column_ref_string.split('\n')]
+COLUMN_REF = {_r[0]: _r[1] for _r in _rows}
 
 
 def load_from_key(key: str,
@@ -20,3 +27,7 @@ def load_from_file(file_path: str,
         string = f.read()
     return load_from_string(string=string,
                             default_database=default_database)
+
+
+def column_reference(ref: str):
+    return COLUMN_REF.get(ref)
